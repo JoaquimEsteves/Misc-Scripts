b79e9caa46c5c9aa1cc2e7d75ba293b28f023e1a
Joaquim Esteves <joaquimbve@hotmail.com>
2021-09-09 12:59:09 +0200

    💅 Added Colors (removed unbuffer)

--- git-summary.sh
+++ git-summary.sh
@@ -1,3 +1,7 @@
-unbuffer git status -sb | head -n 1
-for filter in A C D M R T U X B; do git diff --diff-filter="$filter" --stat | tac | tail -n +2 | tac | sed "s/.*/$filter &/"; done
+# Remember to set [color] diff/status = always in your git config :)
+RED=$(printf '\033[0;31m')
+NC=$(printf '\033[0m') # No Color
+
+git status -sb | head -n 1
+for filter in A C D M R T U X B; do git diff --diff-filter="$filter" --stat | tac | tail -n +2 | tac | sed "s/.*/${RED}${filter}${NC} &/"; done
 git status --porcelain -s | grep "^?? " | sed 's/??/U/'
