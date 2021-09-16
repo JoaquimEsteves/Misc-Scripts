10754cbecf521e9c119e93fadda6eba3f031c962
Joaquim Esteves <joaquimbve@hotmail.com>
2021-09-16 13:42:21 +0200

    ✨ Improved git-summary. Now includes the staged files

--- git-summary.sh
+++ git-summary.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
 # Remember to set [color] diff/status = always in your git config :)
 RED=$(printf '\033[0;31m')
+GREEN=$(printf '\033[0;32m')
 NC=$(printf '\033[0m') # No Color
 
 status=$(git status -sb)
 echo "$status" | head -n 1
-for filter in A C D M R T U X B; do git diff --diff-filter="$filter" --stat | tac | tail -n +2 | tac | sed "s/.*/${RED}${filter}${NC} &/"; done
+for filter in A C D M R T U X B
+    do git diff --staged --diff-filter="$filter" --stat | tac | tail -n +2 | tac | sed "s/.*/${GREEN}${filter}${NC} &/"
+done
+for filter in A C D M R T U X B
+    do git diff --diff-filter="$filter" --stat | tac | tail -n +2 | tac | sed "s/.*/${RED}${filter}${NC} &/"
+done
 echo "$status" | grep "??"
