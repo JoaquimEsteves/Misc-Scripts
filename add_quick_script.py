5a3e9bc94b6b8b1820ddfe4ac2a9dcc815626b4f
Joaquim Esteves <joaquimbve@hotmail.com>
2023-01-17 15:31:25 +0100

    ✨ Allows quick_script to rename the file

--- add_quick_script.py
+++ add_quick_script.py
@@ -6,10 +6,15 @@ import os
 import stat
 from pathlib import Path
 
+YES = ("Y", "y")
+RENAME = ("R", "r")
+
+BIN = Path("~/.local/bin").expanduser()
+
 
 def add_script_to_path(script_path: str) -> int:
-    proper_path = Path(script_path).resolve()
-    final_path = Path("~/.local/bin/" + Path(script_path).stem).expanduser()
+    proper_path = Path(script_path).expanduser().resolve()
+    final_path = BIN / Path(script_path).stem
     if not proper_path.exists():
         raise Exception(f"{proper_path} is not a valid path!")
     if final_path.exists():
@@ -18,15 +23,26 @@ def add_script_to_path(script_path: str) -> int:
         # Give the file the proper access
         proper_path.chmod(proper_path.stat().st_mode | stat.S_IEXEC)
 
+    return io_helper(proper_path, final_path)
+
+
+def io_helper(proper_path: Path, final_path: Path):
     # Finally, create symlink
     print("Creating the following Symlink:")
     print(f"{proper_path} -> {final_path}")
     print("[Y/y]?")
-    if input() not in ("Y", "y"):
+    print("[R/r] rename?")
+    inpt = input()
+    if inpt in YES:
+        os.symlink(proper_path, final_path)
+        return 0
+    if inpt not in RENAME:
         print("Goodbye")
         return 1
-    os.symlink(proper_path, final_path)
-    return 0
+    return io_helper(
+        proper_path,
+        BIN / input("Rename the final bit to:\n"),
+    )
 
 
 if __name__ == "__main__":
